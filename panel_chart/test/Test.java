fb2f506b200b4c41f3f218661e8159661c290883
rizkits <108572307+rizkits@users.noreply.github.com>
2023-12-02 05:36:58 +0700

    Update panel

--- panel_chart/test/Test.java
+++ panel_chart/test/Test.java
@@ -1,9 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ */
 package test;
 
 import java.awt.Color;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -11,77 +14,112 @@ import java.util.List;
 import javax.swing.JOptionPane;
 import raven.chart.ModelChart;
 
+/**
+ *
+ * @author rizki
+ */
 public class Test extends javax.swing.JFrame {
 
+    /**
+     * Creates new form Test
+     */
     public Test() {
         initComponents();
-        chart.setTitle("This is chart");
-        chart.addLegend("Pengeluaran", Color.decode("#56fa56"), Color.decode("#93ab3a"));
-        chart.addLegend("Pemasukan", Color.decode("#ad439b"), Color.decode("#325cdb"));
-//        chart.addLegend("Profit", Color.decode("#77165a"), Color.decode("#83ab12"));
-        setData("keluar");
+        chart.setTitle("Chart Data");
+        chart.addLegend("Transaksi", Color.decode("#56fa56"), Color.decode("#93ab3a"));
+        jButton1.setText("Pengeluaran");
+        jButton2.setText("Pemasukan");
+//        setData("");
     }
-    private void setData(String laporan){
+
+    private void setData(String laporan) {
         try {
-            String datenow=LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM"));
-            List<ModelData> lists=new ArrayList<>();
+            chart.resetChart();
+            String datenow = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM"));
+            List<ModelData> lists = new ArrayList<>();
             DatabaseConnection.getInstance().connectToDatabase();
-            String sql=(laporan.equals("keluar"))?"select DATE_FORMAT(tanggal, '%d') as 'Date', sum(biaya) as biaya from pengeluaran WHERE tanggal BETWEEN \""+datenow+"-01\" AND \""+datenow+"-31\" group by DATE_FORMAT(tanggal, '%d%M') order by tanggal DESC limit 31;":"select DATE_FORMAT(tgl_selesai, '%d') as 'Date', sum(subtotal_harga) as biaya from detail_pesanan WHERE tgl_selesai BETWEEN \""+datenow+"-01\" AND \""+datenow+"-31\" group by DATE_FORMAT(tgl_selesai, '%d%M') order by tgl_selesai DESC limit 31;";
-//            String sql="select DATE_FORMAT(Date, '%M') as 'Month', sum(totalAmount) as Amount, sum(TotalCost) as Cost, sum(totalProfit) as Profit from orders group by DATE_FORMAT(Date, '%m%Y') order by Date DESC limit 7";
-            PreparedStatement pst=DatabaseConnection.getInstance().getConnection().prepareStatement(sql);
-            ResultSet res=pst.executeQuery();
+            String sql = (laporan.equals("keluar")) ? "select DATE_FORMAT(tanggal, '%d') as 'Date', sum(biaya) as biaya from pengeluaran WHERE tanggal BETWEEN \"" + datenow + "-01\" AND \"" + datenow + "-31\" group by DATE_FORMAT(tanggal, '%d%M') order by tanggal DESC limit 31;" : "select DATE_FORMAT(tgl_selesai, '%d') as 'Date', sum(subtotal_harga) as biaya from detail_pesanan WHERE tgl_selesai BETWEEN \"" + datenow + "-01\" AND \"" + datenow + "-31\" group by DATE_FORMAT(tgl_selesai, '%d%M') order by tgl_selesai DESC limit 31;";
+            PreparedStatement p = DatabaseConnection.getInstance().getConnection().prepareStatement(sql);
+            ResultSet res = p.executeQuery();
             while (res.next()) {
-                String month=res.getString("Date");
-//                String month=res.getString("Month");
-//                double amount=res.getDouble("Amount");
-                double amount=res.getInt("biaya");
+                String month = res.getString("Date");
+                double amount = res.getDouble("biaya");
 //                double cost=res.getDouble("Cost");
 //                double profit=res.getDouble("Profit");
                 lists.add(new ModelData(month, amount));
-//                lists.add(new ModelData(month, amount, cost, profit));
             }
             res.close();
-            pst.close();
-            for (int i = lists.size()-1; i >= 0; i--) {
-                ModelData d=lists.get(i);
-                chart.addData(new ModelChart(d.getMonth(), new double[]{
-                    d.getAmount(),
-//                    d.getAmount(),
-//                    d.getCost(),
-//                    d.getProfit()
-                }));
+            p.close();
+            for (int i = lists.size() - 1; i >= 0; i--) {
+                ModelData d = lists.get(i);
+                chart.addData(new ModelChart(d.getMonth(), new double[]{d.getAmount()}));
             }
             chart.start();
-        } catch (SQLException e) {
+        } catch (Exception e) {
             JOptionPane.showMessageDialog(null, e.getMessage());
         }
     }
 
+    /**
+     * This method is called from within the constructor to initialize the form.
+     * WARNING: Do NOT modify this code. The content of this method is always
+     * regenerated by the Form Editor.
+     */
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
         panelShadow1 = new raven.panel.PanelShadow();
         chart = new raven.chart.CurveLineChart();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
+        panelShadow1.setBackground(new java.awt.Color(43, 83, 83));
+        panelShadow1.setBorder(javax.swing.BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        panelShadow1.setColorGradient(new java.awt.Color(22, 55, 55));
+
+        chart.setForeground(new java.awt.Color(231, 231, 231));
         chart.setFillColor(true);
+
+        jButton1.setText("jButton1");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jButton2.setText("jButton1");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout panelShadow1Layout = new javax.swing.GroupLayout(panelShadow1);
         panelShadow1.setLayout(panelShadow1Layout);
         panelShadow1Layout.setHorizontalGroup(
             panelShadow1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(panelShadow1Layout.createSequentialGroup()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelShadow1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(chart, javax.swing.GroupLayout.PREFERRED_SIZE, 775, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addComponent(chart, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelShadow1Layout.createSequentialGroup()
+                .addContainerGap(511, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton2)
+                .addGap(45, 45, 45))
         );
         panelShadow1Layout.setVerticalGroup(
             panelShadow1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(panelShadow1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(chart, javax.swing.GroupLayout.DEFAULT_SIZE, 360, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelShadow1Layout.createSequentialGroup()
+                .addGroup(panelShadow1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton2)
+                    .addComponent(jButton1))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 12, Short.MAX_VALUE)
+                .addComponent(chart, javax.swing.GroupLayout.PREFERRED_SIZE, 412, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
 
@@ -99,6 +137,16 @@ public class Test extends javax.swing.JFrame {
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        setData("keluar");
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        setData("");
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -126,6 +174,9 @@ public class Test extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(Test.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -137,6 +188,8 @@ public class Test extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private raven.chart.CurveLineChart chart;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private raven.panel.PanelShadow panelShadow1;
     // End of variables declaration//GEN-END:variables
 }
